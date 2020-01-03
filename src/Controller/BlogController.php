5bd344e31d3164ddbad95b3bab884843b9352915
Eric <eric.narbo@gmail.com>
2020-01-03 04:37:51 +0100

    on va tester sur une autre branche. Pour crud

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,7 +5,10 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
@@ -34,15 +37,40 @@ class BlogController extends AbstractController
         ]);
     }
 
+     /**
+      * @route("/blog/new" , name="blog_create")
+      */
+      public function create()
+      {
+          $article = new Article();
+
+          $form = $this->createFormBuilder($article)
+                        ->add('title')
+                        ->add('content')
+                        ->add('image')
+                        ->getForm();
+
+        var_dump($article);
+          
+          
+          
+          return $this->render('blog/create.html.twig', [
+              'formArticle' => $form->createView()
+          ]);
+      }
+
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
 
-     public function show(Article $article){
+     public function show(Article $article)
+     {
          
-
          return $this->render('blog/show.html.twig', [
              'article' => $article
          ]);
      }
+
+    
+
 }
