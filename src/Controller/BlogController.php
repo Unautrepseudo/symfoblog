e1d48964395df2557b570fce6d8651634911ccd0
Eric <eric.narbo@gmail.com>
2020-01-03 01:42:40 +0100

    blog skeleton

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index()
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render('blog/home.html.twig', [
+            'title' => "Bienvenue sur ce suuuper blog"
+        ]);
+    }
+
+    /**
+     * @Route("/blog/12", name="blog_show")
+     */
+
+     public function show(){
+         return $this->render('blog/show.html.twig');
+     }
+}
